c97ae1d35baad0cf9926952c7c45a4ba670c8945
anikayt17 <77901887+anikayt17@users.noreply.github.com>
2021-02-12 13:43:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let urls = ['92511547', '87768382', '87275560', '87771926', '28132358', '91283844'];
+let urls = ['92511547', '28132358', '91283844'];
 for(let i = 0; i<urls.length; i++) {
 	if(window.location.href.includes(urls[i])) {
 		let node = document.getElementsByClassName('flag-lgtb')[0].parentElement;
